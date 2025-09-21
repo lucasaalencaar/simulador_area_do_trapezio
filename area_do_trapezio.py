3f678fd9179937097b9406714ebaa1883438509c
Lucas de Alencar <lucasalvesalencar3@gmail.com>
2025-09-21 12:35:28 -0300

    Add files via upload

---
+++ area_do_trapezio.py
@@ -0,0 +1,6 @@
+print('===Fórmula da área do trapézio===')
+base_maior = float(input('Digite o valor da base maior: '))
+base_menor = float(input('Digite o valor da base menor: '))
+altura = float(input('Digite o valor da altura: '))
+area = (base_maior + base_menor) * altura/2
+print(f'O resultado da área do trapézio é igual a {area}' )
